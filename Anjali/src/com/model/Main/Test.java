a718cc36f30acf8841aa2f8239c1b7c84cd7694d
Admin <Admin@DESKTOP-1UAFC8K>
2024-05-14 17:01:33 +0530

    commite from anjali

---
+++ Anjali/src/com/model/Main/Test.java
@@ -0,0 +1,17 @@
+package com.model.Main;
+
+import com.example.model.Student;
+
+public class Test 
+{
+   public void m1()
+   {
+	   Student s =new Student();
+	   
+   }
+	public static void main(String[] args) 
+	{
+	   Test t =new Test();	
+	}
+
+}
